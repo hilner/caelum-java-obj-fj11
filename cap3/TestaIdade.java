a1174b5930e25ec98d1629bd8a856941ff64f147
Emiliano Carvalho <emilianojpa@gmail.com>
2013-12-31 18:13:57 -0300

    Iniciando variáveis primitivas e controle de fluxo

---
+++ cap3/TestaIdade.java
@@ -0,0 +1,16 @@
+class TestaIdade {
+  public static void main (String[] args){
+
+    //declara a variavel idade
+    int idade = 20;
+
+    System.out.println(idade);
+
+    int idadeNoAnoQueVem;
+    idadeNoAnoQueVem = idade + 1;
+
+    System.out.println(idadeNoAnoQueVem);
+  
+  }
+
+}
