30421c8588cb6318bc5600b6839ad3e28362aab6
Emiliano Carvalho <emilianojpa@gmail.com>
2013-12-31 19:07:46 -0300

    Tipos primitivos

--- cap3/TestaIdade.java
+++ cap3/TestaIdade.java
@@ -11,6 +11,44 @@ class TestaIdade {
 
     System.out.println(idadeNoAnoQueVem);
   
+    // valores reais
+
+    double pi = 3.14;
+    double x = 5 * 10;
+
+    System.out.println("Valores reais");
+    System.out.println(pi);
+    System.out.println(x);
+
+    // booleano
+    
+    boolean menorDeIdade = idade < 18;
+    System.out.println("variaveis booleanas");
+    System.out.println(menorDeIdade);
+
+
+    // tipo char - armazena apenas um caracter
+    
+    char letra = 'a';
+    System.out.println("variaveis char");
+    System.out.println(letra);
+
+
+    // tipos primitivos e valores
+    
+    int i = 5; // i recebe uma copia do valor de 5
+    int j = i; // j recebe uma copia do valor de i
+    System.out.println("Tipos primitivos e valores");
+    System.out.println("Valor de i " + i);
+    System.out.println("Valor de j = i " + j);
+
+    i = i + 1; // i vira 6 e j continua: 5
+
+    System.out.println("i = i + 1");
+    System.out.print(i);
+    System.out.println("O valor de j nao foi alterado "+ j);
+
+
   }
 
 }
