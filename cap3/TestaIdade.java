ecfcb20503af068e35d93abda0f7d0d07caa7caa
Emiliano Carvalho <emilianojpa@gmail.com>
2013-12-31 19:35:41 -0300

    Tipos primitivos - complemento

--- cap3/TestaIdade.java
+++ cap3/TestaIdade.java
@@ -39,15 +39,30 @@ class TestaIdade {
     int i = 5; // i recebe uma copia do valor de 5
     int j = i; // j recebe uma copia do valor de i
     System.out.println("Tipos primitivos e valores");
-    System.out.println("Valor de i " + i);
-    System.out.println("Valor de j = i " + j);
+    System.out.println("Valor de i -> " + i);
+    System.out.println("Valor de j = i -> " + j);
 
     i = i + 1; // i vira 6 e j continua: 5
 
-    System.out.println("i = i + 1");
-    System.out.print(i);
-    System.out.println("O valor de j nao foi alterado "+ j);
+    System.out.print("i = i + 1 ->");
+    System.out.println(i);
+    System.out.println("O valor de j nao foi alterado ->"+ j);
 
+    // outros tipos
+    byte b = 127;
+    short s = 32767;
+    int n = 2147483647;
+    long l = 1000000;
+    float f = 100000000;
+    double d = 1.7E+38;
+
+    System.out.println("Outros tipos primitivos");
+    System.out.println("Byte (inteiro: -128 a 127) b -> "+ b);
+    System.out.println("Short (inteiro: -32768 a 32767) s -> "+ s);
+    System.out.println("Int (inteiro: -2147483648 a 2147483647) n -> "+ n);
+    System.out.println("Long (inteiro: -9223372036854775808 a 9223372036854775807) l -> "+ l);
+    System.out.println("Float (inteiro:  +/-3.4E-38 a +/-3.4E+38) f -> "+ f);
+    System.out.println("Double (inteiro:  +/-1.7E-38 a +/-1.7E+38) d -> "+ d);
 
   }
 
