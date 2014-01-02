c3e2cff04ffad587caa531a6cf35f4f539d7542f
Emiliano Carvalho <emilianojpa@gmail.com>
2014-01-01 23:45:40 -0300

    O IF e ELSE

---
+++ cap3/Condicionais.java
@@ -0,0 +1,16 @@
+class Condicionais {
+
+  public static void main(String[] args){
+ 
+    // if e else
+    int idade = 15;
+    if (idade < 18) {
+      System.out.println("Nao pode entrar!");
+    
+    }
+
+
+
+  }
+
+}
