c51f5f31382e46a49abb6ad89e39d627bb0f0f80
Emiliano Carvalho <emilianojpa@gmail.com>
2014-01-02 00:01:36 -0300

    Estruturas de repetição - While

---
+++ cap3/Repeticoes.java
@@ -0,0 +1,15 @@
+class Repeticoes {
+
+  public static void main(String[] args){
+ 
+    // while
+    int idade = 15;
+    while (idade < 18) {
+      System.out.println(idade);
+      idade = idade + 1; // ou idade++
+
+    }
+
+  }
+
+}
