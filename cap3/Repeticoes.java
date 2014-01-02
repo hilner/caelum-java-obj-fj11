bfe85e784ac5f82cc3161f7ae31d9179d95f29e6
Emiliano Carvalho <emilianojpa@gmail.com>
2014-01-02 00:04:03 -0300

    Repetições outro exemplo - While

--- cap3/Repeticoes.java
+++ cap3/Repeticoes.java
@@ -7,7 +7,13 @@ class Repeticoes {
     while (idade < 18) {
       System.out.println(idade);
       idade = idade + 1; // ou idade++
+    }
 
+    int i = 0;
+    while (i < 10) {
+      System.out.println(i);
+      i++;
+    
     }
 
   }
