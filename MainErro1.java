e28feeafe9ce88ed9f279f526816e572532525f8
Emiliano Carvalho <emilianojpa@gmail.com>
2014-07-18 19:58:03 -0300

    Exercícios iniciais

---
+++ MainErro1.java
@@ -0,0 +1,5 @@
+class MainErro1 {
+	public void main(String[] args) {
+		System.out.println("Faltou o static, tente executar!\n");
+	}
+}
---
+++ ParaSaberMais1.java
@@ -0,0 +1,9 @@
+class ParaSaberMais1 {
+	public static void main (String[] args) {
+		int i;
+		for (i = 0; i < 10; ++i) {
+			System.out.println("Olá!");
+		}
+		System.out.println(i);
+	}
+}
