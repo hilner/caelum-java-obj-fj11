7b0cade87ed4e2079e5212a16c77fcc3a3d04373
Emiliano Carvalho <emilianojpa@gmail.com>
2014-07-19 19:32:32 -0300

    Referenciando a classe cliente

---
+++ cap4/RefOutraClass.java
@@ -0,0 +1,56 @@
+class RefOutraClass {
+
+	public static class Cliente {
+		String nome;
+		String sobrenome;
+		String cpf;
+	}
+
+	public static class Conta {
+		int numero;
+		Cliente titular;
+		double saldo;
+		double limite;
+
+		boolean saca (double valor) {
+			if (this.saldo < valor) {
+				return false;
+			}
+			else {
+				this.saldo = this.saldo - valor;
+				return true;
+			}
+		}
+
+		void deposita (double quantidade) {
+			this.saldo += quantidade;
+		}
+
+		boolean transferePara(Conta destino, double valor) {
+			boolean retirou = this.saca(valor);
+			if (retirou) {
+				// sem sacar
+				return false;
+			} else {
+				destino.deposita(valor);
+				return true;
+			}
+		}
+	}
+
+	public static void main (String[] args) {
+		Conta c1 = new Conta();
+		Cliente cliente = new Cliente();
+		c1.titular = cliente;
+
+		c1.titular.nome = "Duke";
+		c1.titular.sobrenome = "Donald";
+		c1.saldo = 5000;
+
+
+		System.out.println("Titular ");
+		System.out.println("Nome = " + c1.titular.nome);
+		System.out.println("Sobrenome = " + c1.titular.sobrenome);
+
+	}
+}
