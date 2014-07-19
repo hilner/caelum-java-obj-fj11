89597841d296c0e7511c4bb2aff38a9577ebbe5c
Emiliano Carvalho <emilianojpa@gmail.com>
2014-07-19 15:57:23 -0300

    Adicionado métodos saca e deposita

--- cap4/Programa.java
+++ cap4/Programa.java
@@ -10,16 +10,34 @@ class Programa {
 			double novoSaldo = this.saldo - quantidade;
 			this.saldo = novoSaldo;
 		}
+
+		void deposita (double quantidade) {
+			this.saldo += quantidade;
+		}
 	}
 
 	public static void main (String[] args) {
+		// criando a conta
 		Conta minhaConta;
 		minhaConta = new Conta();
 
+		// alterando os valores de minhaConta
 		minhaConta.dono = "Duke";
 		minhaConta.saldo = 1000.0;
 
-		System.out.println("Saldo atual: " + minhaConta.saldo);
+		System.out.println("Saldo inicial: " + minhaConta.saldo);
+
+		// saca 200 reais
+		minhaConta.saca(200);
+
+		System.out.println("Retirada: " + 200 + " reais");
+		System.out.println("Saldo atualizado: " + minhaConta.saldo);
+
+		// deposita 500 reais
+		minhaConta.deposita(500);
+
+		System.out.println("Deposito: " + 500 + " reais");
+		System.out.println("Saldo atualizado: " + minhaConta.saldo);
 
 	}
 }
