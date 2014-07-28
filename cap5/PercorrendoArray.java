e0380bacc4a5d85cf19dca4d244166a63bc75b8e
Emiliano Carvalho <emilianojpa@gmail.com>
2014-07-27 23:34:18 -0300

    Como implementar o recebimento de uma array como argumento de um método?

--- cap5/PercorrendoArray.java
+++ cap5/PercorrendoArray.java
@@ -1,4 +1,10 @@
 class PercorrendoArray {
+	void imprimeArray(int[] array) {
+		// não compila !!
+		for (int i = 0; i < ????; ++i) {
+			System.out.println(array[i]);
+		}
+	}
 	public static void main(String[] args) {
 		int[] idades = new int[10];
 		for (int i = 0; i < 10; ++i) {
