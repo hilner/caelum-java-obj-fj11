49d0848bb07679c3c30e5473a72ea89d84779024
Emiliano Carvalho <emilianojpa@gmail.com>
2014-07-27 23:01:46 -0300

    Introdução arrays

---
+++ cap5/Basico.java
@@ -0,0 +1,20 @@
+class Basico {
+	public static void main(String[] args) {
+		int idade1 = 20;
+		int idade2 = 21;
+		int idade3 = 22;
+		int idade4 = 23;
+
+		// com arrays
+
+		int[] idades; // tipo int[]
+
+		idades = new int[10];
+		idades[5] = 10;
+
+		System.out.println("Idade do elemento " + 5 + " é " + idades[5]);
+
+
+	}
+	
+}
