22f6d3da048205ee106758902f3dcabe80ad0830
Emiliano Carvalho <emilianojpa@gmail.com>
2014-07-29 19:12:12 -0300

    Criando a classe TestaEmpresa com constructor na classe Empresa

--- cap5/Empresa.java
+++ cap5/Empresa.java
@@ -1,4 +1,17 @@
 class Empresa {
 	Funcionario[] empregados;
 	String cnpj;
+	int tam;
+
+	// constructor
+	public Empresa () {
+		tam = 0;
+		empregados = new Funcionario[10];
+	}
+
+	void adiciona(Funcionario f) {
+		this.empregados[tam] = f;
+		this.empregados[tam].mostra();
+		tam++;
+	}
 }
---
+++ cap5/TestaEmpresa.java
@@ -0,0 +1,18 @@
+class TestaEmpresa {
+	public static void main(String[] args) {
+		Empresa emp = new Empresa();
+		emp.empregados = new Funcionario[10];
+		Funcionario f1 = new Funcionario();
+		f1.nome = "José";
+		f1.departamento = "Fisica";
+		f1.salario = 100;
+		f1.dataEntrada.dia = 21;
+		f1.dataEntrada.mes = 1;
+		f1.dataEntrada.ano = 2014; 
+		f1.rg = "1111111";
+		f1.estaNaEmpresa = true;
+
+		emp.adiciona(f1);
+
+	}
+}
