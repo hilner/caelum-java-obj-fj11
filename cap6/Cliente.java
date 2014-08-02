be19e3a831df1561f8f2d890e50116f9fe5c50b3
Emiliano Carvalho <emilianojpa@gmail.com>
2014-08-02 14:17:50 -0300

    Implementando a solução para o problema da validade do CPF

--- cap6/Cliente.java
+++ cap6/Cliente.java
@@ -1,6 +1,15 @@
 class Cliente {
+	private String nome;
+	private String sobrenome;
+	private String cpf;
+	private int idade;
 
-	String nome;
-	String sobrenome;
-	String cpf;
+	public void mudaCPF(String cpf) {
+		validaCPF(cpf);
+		this.cpf = cpf;
+	}
+
+	private void validaCPF(String cpf) {
+		// implementação 
+	}
 }
