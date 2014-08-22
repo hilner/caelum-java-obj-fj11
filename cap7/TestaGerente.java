6b38eb09268f42a3491c4617ea1d208dfeefa308
Emiliano Carvalho <emilianojpa@gmail.com>
2014-08-22 00:21:53 -0300

    Gerente sem reescrita, recebendo a mesma bonificacao

--- cap7/TestaGerente.java
+++ cap7/TestaGerente.java
@@ -8,5 +8,9 @@ class TestaGerente {
 		// e também métodos do Gerente!
 		gerente.setSenha(4321);
 		gerente.autentica(4321);
+
+		gerente.setSalario(5000.0);
+		gerente.getBonificacao();
+		System.out.println(gerente.getBonificacao());
 	}
 }
